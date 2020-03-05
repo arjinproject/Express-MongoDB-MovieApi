1182cf7abf24e67fa5547e073b7575608f1c14a1
Hamit SEYREK <hamitseyrek@gmail.com>
2020-03-05 13:39:13 +0300

    connect mongoDb

---
+++ helper/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect("mongodb+srv://hmtsyrk:426485.h-m@cluster0-2rrr3.mongodb.net/test?retryWrites=true&w=majority", { useMongoClient: true });
+    mongoose.connection.on("open", () => {
+        console.log("MongoDb Connected");
+    });
+    mongoose.connection.on("error", () => {
+        console.log("MongoDb Error: ", error);
+    });
+};
